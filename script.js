7162a45c352944a2bdb88f0ecd7e9471ec6b0040
matyaskova <43047469+matyaskova@users.noreply.github.com>
2024-09-23 11:37:00 +0200

    funkce

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ let datum = new Date()
 let den = datum.getDate()
 let mesic = datum.getMonth()+1
 let rok = datum.getFullYear()
-document.write("Dnes je "+den+". "+mesic+". "+rok)
+
+document.getElementById("datum").innerText="Dnes je "+den+". "+mesic+". "+rok
+
 let hodina = datum.getHours()
 let minuta = datum.getMinutes()
 let sekunda = datum.getSeconds()
